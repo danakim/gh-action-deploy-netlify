f4c43ef301058e18f8bf5dbf3c592c2fa173c586
Dan <dan@hostatic.ro>
2020-04-10 16:52:25 +0300

    This should be the proper directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 AUTH_TOKEN=$1
 SITE_ID=$2
 DEPLOY_PROD=$3
-BUILD_DIRECTORY='/build'
+BUILD_DIRECTORY='/github/home/build'
 
 if [ -z $DEPLOY_PROD ]; then
     netlify deploy --dir ${BUILD_DIRECTORY} --site=${SITE_ID} --auth=${AUTH_TOKEN} --timeout=600 --message "Deployed on $(date)"
